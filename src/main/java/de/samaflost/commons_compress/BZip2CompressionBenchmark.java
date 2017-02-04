71fb929d7183d97f9b1b62b04e4966c38652926d
Stefan Bodewig <bodewig@apache.org>
2017-02-04 20:37:11 +0100

    initialize using the JMH way

--- src/main/java/de/samaflost/commons_compress/BZip2CompressionBenchmark.java
+++ src/main/java/de/samaflost/commons_compress/BZip2CompressionBenchmark.java
@@ -9,25 +9,28 @@ import java.net.URL;
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.commons.compress.utils.IOUtils;
-import org.openjdk.jmh.annotations.Benchmark;
 
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+@State(Scope.Benchmark)
 public class BZip2CompressionBenchmark {
 
-    private static final byte[] SMALL_FILE;
-    private static final byte[] BIGGER_FILE;
+    private final byte[] SMALL_FILE;
+    private final byte[] BIGGER_FILE;
 
-    static {
-        try {
-            try (InputStream in = BZip2CompressionBenchmark.class.getResourceAsStream("/bla.tar")) {
-                SMALL_FILE = IOUtils.toByteArray(in);
-            }
-            try (InputStream fromUrl = new URL("http://archive.apache.org/dist/commons/compress/source/commons-compress-1.13-src.tar.gz")
-                     .openStream();
-                 GzipCompressorInputStream gzIn = new GzipCompressorInputStream(fromUrl)) {
-                BIGGER_FILE = IOUtils.toByteArray(gzIn);
-            }
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
+    @Setup
+    public void readData() throws Exception {
+        System.err.println("init");
+        try (InputStream in = BZip2CompressionBenchmark.class.getResourceAsStream("/bla.tar")) {
+            SMALL_FILE = IOUtils.toByteArray(in);
+        }
+        try (InputStream fromUrl = new URL("http://archive.apache.org/dist/commons/compress/source/commons-compress-1.13-src.tar.gz")
+                 .openStream();
+             GzipCompressorInputStream gzIn = new GzipCompressorInputStream(fromUrl)) {
+            BIGGER_FILE = IOUtils.toByteArray(gzIn);
         }
     }
 
