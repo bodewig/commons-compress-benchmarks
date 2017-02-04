3a63ff58dddb150c5a45aa9b1c0e750459aa0456
Stefan Bodewig <bodewig@apache.org>
2017-02-04 20:13:23 +0100

    test with a bigger file to compress

--- src/main/java/de/samaflost/commons_compress/BZip2CompressionBenchmark.java
+++ src/main/java/de/samaflost/commons_compress/BZip2CompressionBenchmark.java
@@ -4,19 +4,27 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.commons.compress.utils.IOUtils;
 import org.openjdk.jmh.annotations.Benchmark;
 
 public class BZip2CompressionBenchmark {
 
     private static final byte[] SMALL_FILE;
+    private static final byte[] BIGGER_FILE;
 
     static {
         try {
             try (InputStream in = BZip2CompressionBenchmark.class.getResourceAsStream("/bla.tar")) {
                 SMALL_FILE = IOUtils.toByteArray(in);
+            }
+            try (InputStream fromUrl = new URL("http://archive.apache.org/dist/commons/compress/source/commons-compress-1.13-src.tar.gz")
+                     .openStream();
+                 GzipCompressorInputStream gzIn = new GzipCompressorInputStream(fromUrl)) {
+                BIGGER_FILE = IOUtils.toByteArray(gzIn);
             }
         } catch (Exception ex) {
             throw new RuntimeException(ex);
@@ -25,9 +33,18 @@ public class BZip2CompressionBenchmark {
 
     @Benchmark
     public void compressSmallFile() throws IOException {
+        compress(SMALL_FILE);
+    }
+
+    @Benchmark
+    public void compressBiggerFile() throws IOException {
+        compress(BIGGER_FILE);
+    }
+
+    private void compress(byte[] data) throws IOException {
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
              BZip2CompressorOutputStream bout = new BZip2CompressorOutputStream(baos);
-             ByteArrayInputStream in = new ByteArrayInputStream(SMALL_FILE)) {
+             ByteArrayInputStream in = new ByteArrayInputStream(data)) {
             IOUtils.copy(in, bout);
         }
     }
