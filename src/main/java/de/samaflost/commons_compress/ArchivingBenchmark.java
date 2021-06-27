6a0299b46c6ff3be29c8cdb78b8f2ad3bb0ed4bb
Stefan Bodewig <stefan.bodewig@innoq.com>
2021-06-27 11:35:30 +0200

    benchmarks for archivers as well

---
+++ src/main/java/de/samaflost/commons_compress/ArchivingBenchmark.java
@@ -0,0 +1,123 @@
+package de.samaflost.commons_compress;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.ar.ArArchiveEntry;
+import org.apache.commons.compress.archivers.cpio.CpioArchiveEntry;
+import org.apache.commons.compress.archivers.sevenz.SevenZArchiveEntry;
+import org.apache.commons.compress.archivers.sevenz.SevenZOutputFile;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.utils.IOUtils;
+import org.apache.commons.compress.utils.SeekableInMemoryByteChannel;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+@State(Scope.Benchmark)
+public class ArchivingBenchmark {
+
+    private byte[] SMALL_FILE;
+    private byte[] BIGGER_FILE;
+    private static final ArchiveStreamFactory factory = new ArchiveStreamFactory();
+
+    @Param({"zip"})
+    public String format;
+
+    @Setup
+    public void readData() throws Exception {
+        SMALL_FILE = TestFixture.SMALL_FILE;
+        BIGGER_FILE = TestFixture.BIGGER_FILE;
+    }
+
+    @Benchmark
+    public byte[] archiveSmallFile() throws Exception {
+        return archive(SMALL_FILE);
+    }
+
+    @Benchmark
+    public byte[] archiveManySmallFiles() throws Exception {
+        return archive(SMALL_FILE, format, 10000);
+    }
+
+    @Benchmark
+    public byte[] archiveBiggerFile() throws Exception {
+        return archive(BIGGER_FILE);
+    }
+
+    private byte[] archive(byte[] data) throws Exception {
+        return archive(data, format, 1);
+    }
+
+    static byte[] archive(byte[] data, String format, int numberOfEntries) throws Exception {
+        switch(format) {
+        case ArchiveStreamFactory.SEVEN_Z:
+            return archive7z(data, format, numberOfEntries);
+        default:
+            return archiveStream(data, format, numberOfEntries);
+        }
+    }
+
+    private static byte[] archiveStream(byte[] data, String format, int numberOfEntries) throws Exception {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+             ArchiveOutputStream bout = factory.createArchiveOutputStream(format, baos);
+             ByteArrayInputStream in = new ByteArrayInputStream(data)) {
+            for (int i = 0; i < numberOfEntries; i++) {
+                bout.putArchiveEntry(createEntry(format, i, data.length));
+                IOUtils.copy(in, bout);
+                bout.closeArchiveEntry();
+            }
+            bout.finish();
+            bout.close();
+            return baos.toByteArray();
+        }
+    }
+
+    private static byte[] archive7z(byte[] data, String format, int numberOfEntries) throws Exception {
+        try (SeekableInMemoryByteChannel ch = new SeekableInMemoryByteChannel();
+             SevenZOutputFile bout = new SevenZOutputFile(ch);
+             ByteArrayInputStream in = new ByteArrayInputStream(data)) {
+            final byte[] buffer = new byte[8192];
+            for (int i = 0; i < numberOfEntries; i++) {
+                bout.putArchiveEntry(createEntry(format, i, data.length));
+                int n = 0;
+                while (-1 != (n = in.read(buffer))) {
+                    bout.write(buffer, 0, n);
+                }
+                bout.closeArchiveEntry();
+            }
+            bout.finish();
+            bout.close();
+            return Arrays.copyOf(ch.array(), (int) ch.size());
+        }
+    }
+
+    private static ArchiveEntry createEntry(String format, int entryNumber, long length) {
+        String entryName = format + "." + entryNumber;
+        switch(format) {
+        case ArchiveStreamFactory.AR:
+            return new ArArchiveEntry(entryName, length);
+        case ArchiveStreamFactory.CPIO:
+            return new CpioArchiveEntry(entryName, length);
+        case ArchiveStreamFactory.JAR:
+        case ArchiveStreamFactory.ZIP:
+            return new ZipArchiveEntry(entryName);
+        case ArchiveStreamFactory.TAR:
+            return new TarArchiveEntry(entryName);
+        case ArchiveStreamFactory.SEVEN_Z:
+            SevenZArchiveEntry e = new SevenZArchiveEntry();
+            e.setName(entryName);
+            return e;
+        default:
+            throw new RuntimeException("unsupported format " + format);
+        }
+    }
+}
---
+++ src/main/java/de/samaflost/commons_compress/UnarchivingBenchmark.java
@@ -0,0 +1,96 @@
+package de.samaflost.commons_compress;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.sevenz.SevenZFile;
+import org.apache.commons.compress.utils.IOUtils;
+import org.apache.commons.compress.utils.SeekableInMemoryByteChannel;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+@State(Scope.Benchmark)
+public class UnarchivingBenchmark {
+
+    private byte[] SMALL_FILE;
+    private byte[] MANY_SMALL_FILES;
+    private byte[] BIGGER_FILE;
+    private static final ArchiveStreamFactory factory = new ArchiveStreamFactory();
+
+    @Param({"zip"})
+    public String format;
+
+    @Setup
+    public void readData() throws Exception {
+        SMALL_FILE = ArchivingBenchmark.archive(TestFixture.SMALL_FILE, format, 1);
+        BIGGER_FILE = ArchivingBenchmark.archive(TestFixture.BIGGER_FILE, format, 1);
+        MANY_SMALL_FILES = ArchivingBenchmark.archive(TestFixture.SMALL_FILE, format, 10000);
+    }
+
+    @Benchmark
+    public byte[] unarchiveSmallFile() throws Exception {
+        return unarchive(SMALL_FILE);
+    }
+
+    @Benchmark
+    public byte[] unarchiveManySmallFiles() throws Exception {
+        return unarchive(MANY_SMALL_FILES);
+    }
+
+    @Benchmark
+    public byte[] archiveBiggerFile() throws Exception {
+        return unarchive(BIGGER_FILE);
+    }
+
+    private byte[] unarchive(byte[] data) throws Exception {
+        switch(format) {
+        case ArchiveStreamFactory.SEVEN_Z:
+            return unarchive7z(data);
+        default:
+            return unarchiveStream(data);
+        }
+    }
+
+    private byte[] unarchiveStream(byte[] data) throws Exception {
+        try (ByteArrayInputStream in = new ByteArrayInputStream(data);
+             ArchiveInputStream ain = factory.createArchiveInputStream(format, in);
+             ) {
+            byte[] lastEntryData = null;
+            while (ain.getNextEntry() != null) {
+                try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                    IOUtils.copy(ain, out);
+                    out.close();
+                    lastEntryData = out.toByteArray();
+                }
+            }
+            return lastEntryData;
+        }
+    }
+
+    private byte[] unarchive7z(byte[] data) throws Exception {
+        try (SeekableInMemoryByteChannel ch = new SeekableInMemoryByteChannel(data);
+             SevenZFile ain = new SevenZFile(ch);
+             ) {
+            final byte[] buffer = new byte[8192];
+            byte[] lastEntryData = null;
+            while (ain.getNextEntry() != null) {
+                try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                    int n = 0;
+                    while (-1 != (n = ain.read(buffer))) {
+                        out.write(buffer, 0, n);
+                    }
+                    out.close();
+                    lastEntryData = out.toByteArray();
+                }
+            }
+            return lastEntryData;
+        }
+    }
+}
